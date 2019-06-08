8f0193341dbb35bc613c86a411a92d155d51dafc
Ezequiel Marchena <ezmg92@gmail.com>
2019-06-08 23:07:59 +0200

    Cambios en la vista index de horarios.

--- views/horarios/index.php
+++ views/horarios/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\HorariosSearch */
@@ -10,25 +10,34 @@ use yii\grid\GridView;
 $this->title = 'Horarios';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <div class="horarios-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        // 'filterModel' => $searchModel,
-
-        'columns' => [
-
-            'nombreDia.dia',
-            'apertura',
-            'cierre',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
+<table class="table horario">
+        <thead>
+            <tr>
+                <td></td>
+                <?php foreach ($dataProvider->models as $key => $value) : ?>
+                    <th><?= Html::a($value->nombreDia->dia, ['update', 'id' => $value->id], ['data-toggle'=>"tooltip", 'title'=>"Modificar horario"]) ?></th>
+                <?php endforeach ?>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>Apertura</th>
+                <?php foreach ($dataProvider->models as $key => $value) : ?>
+                    <td><?= $value->apertura ?></td>
+                <?php endforeach ?>
+            </tr>
+            <tr>
+                <th>Cierre</th>
+                <?php foreach ($dataProvider->models as $key => $value) : ?>
+                    <td><?= $value->cierre ?></td>
+                <?php endforeach ?>
+            </tr>
+        </tbody>
+    </table>
 
 </div>
